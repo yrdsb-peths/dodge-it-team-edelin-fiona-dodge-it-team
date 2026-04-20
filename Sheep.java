887e7a20a47744083d3a660f23a870a5e7fad7bd
TinaGaohub <440020959@gapps.yrdsb.ca>
2026-04-20 11:34:49 -0400

    add Sheep

---
+++ Sheep.java
@@ -0,0 +1,14 @@
+import greenfoot.*;  
+public class Sheep extends Actor
+{
+    boolean atTop = true;
+    public void act()
+    {
+        if(Greenfoot.mouseClicked(null)){
+            atTop = !atTop;
+        }
+        if(atTop){
+            setLocation(100,100);
+        }
+    }
+}
